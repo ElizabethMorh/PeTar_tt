ada2e2119f098a671ee0f49322a961c3ea58d265
Elizabeth <effymorh@Elizabeth.local>
2025-08-25 17:05:26 +0800

    Hardcoded TSTAR removed, Correct scaling using rscale and vscale

--- src/external_tensor_force.hpp
+++ src/external_tensor_force.hpp
@@ -7,10 +7,8 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <cctype>
 
-const double TSTAR = 0.00449830997959438; // G in PeTar astro units
-
-// PeTar uses PS::F64vec for vectors
 #ifndef PS_F64VEC_DEFINED
 #define PS_F64VEC_DEFINED
 namespace PS {
@@ -25,6 +23,8 @@ namespace PS {
 }
 #endif
 
+template<typename T> class IOParams; // Declare PeTar's IOParams store type
+
 class TidalTensorManager {
 public:
     struct TensorSnapshot {
@@ -34,28 +34,34 @@ public:
 
 private:
     std::vector<TensorSnapshot> snapshots;
-    double last_query_time = 0.0;
-    TensorSnapshot interp; // last interpolated tensor
-    bool loaded = false;
-    PS::F64vec ref_point{0.0,0.0,0.0};
+    double last_query_time_ = 0.0;
+    TensorSnapshot interp_; // last interpolated tensor
+    bool loaded_ = false;
+    PS::F64vec ref_point_{0.0,0.0,0.0};
 
-    // Header values
-    int NBTT = 0;
-    double TTUNIT = 0.0;
-    double TTOFFSET = 0.0;
-    double scale_fac = 1.0;
+    // Header as in NBODY6tt style
+    int    NBTT_     = 0;    // number of rows expected
+    double TTUNIT_   = 1.0;  // time unit of the file's time column, in "TT time units"
+    double TTOFFSET_ = 0.0;  // time offset in "TT time units"
+
+    // Scaling between IN (PeTar) and TT systems provided by the user
+    double rscale_ = 1.0;
+    double vscale_ = 1.0;
 
 public:
     TidalTensorManager() {
-        interp.time = 0.0;
-        for(int i=0;i<3;i++) for(int j=0;j<3;j++) interp.T[i][j]=0.0;
+        interp_.time = 0.0;
+        for (int i=0;i<3;i++) for(int j=0;j<3;j++) interp_.T[i][j]=0.0;
     }
 
-    bool isLoaded() const { return loaded; }
+    bool isLoaded() const { return loaded_; }
+    void setScales(double rscale, double vscale) {
+        rscale_ = rscale;
+        vscale_ = vscale;
+    }
 
     // loadFromFile: read nbtt, ttunit, ttoffset and NBODY6-style table
-    // TSTAR: PeTar time unit (same units as PeTar's internal time).
-    bool loadFromFile(const std::string &filename, double TSTAR) {
+    bool loadFromFile(const std::string &filename) {
         snapshots.clear();
         FILE *fp = std::fopen(filename.c_str(), "r");
         if (!fp) {
@@ -63,37 +69,31 @@ public:
             return false;
         }
 
-        if (std::fscanf(fp, "%d %lf %lf", &NBTT, &TTUNIT, &TTOFFSET) != 3) {
+        if (std::fscanf(fp, "%d %lf %lf", &NBTT_, &TTUNIT_, &TTOFFSET_) != 3) {
             std::cerr << "[TT] ERROR: failed to read header (NBTT TTUNIT TTOFFSET)\n";
             std::fclose(fp);
             return false;
         }
-
-        if (NBTT <= 0) {
+        if (NBTT_ <= 0) {
             std::cerr << "[TT] ERROR: NBTT <= 0 in tt.dat\n";
             std::fclose(fp);
             return false;
         }
 
-        // scaling factor for tensors (NBODY6 logic)
-        scale_fac = (TSTAR / TTUNIT) * (TSTAR / TTUNIT);
+        const double tt_to_in_time = (vscale_ / rscale_) * TTUNIT_; 
+        const double tens_scale    = (rscale_ / vscale_) * (rscale_ / vscale_);
+        snapshots.reserve(std::max(1, NBTT_));
 
-        // pre-reserve
-        snapshots.reserve(std::max(1, NBTT));
-
-        for (int k=0; k<NBTT; ++k) {
-            // We'll attempt to read a line with either 10 columns (time + 9) or 7 columns (time + 6 symmetric)
-            // read as a whole line and parse with sscanf to be robust against different whitespace
+        for (int k=0; k<NBTT_; ++k) {
             char linebuf[1024];
             if (!std::fgets(linebuf, sizeof(linebuf), fp)) break; // EOF or error
 
-            // skip empty or comment lines
+            // skip empty or comment
             char *ptr = linebuf;
-            while(*ptr && isspace(*ptr)) ++ptr;
-            if (*ptr == '\0' || *ptr == '#') { --k; continue; } // ignore and retry same k
+            while (*ptr && std::isspace(*ptr)) ++ptr;
+            if (*ptr=='\0' || *ptr=='#') { --k; continue; } // ignore and retry same k
 
-            double t;
-            double vals[9];
+            double t; double vals[9];
             int nread = std::sscanf(linebuf,
                                     "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
                                     &t,
@@ -101,30 +101,29 @@ public:
                                     &vals[3], &vals[4], &vals[5],
                                     &vals[6], &vals[7], &vals[8]);
 
-            TensorSnapshot s;
+            TensorSnapshot s{};
             if (nread == 10) {
-                s.time = t * (TTUNIT / TSTAR) + (TTOFFSET / TSTAR);
-                s.T[0][0] = vals[0] * scale_fac; s.T[0][1] = vals[1] * scale_fac; s.T[0][2] = vals[2] * scale_fac;
-                s.T[1][0] = vals[3] * scale_fac; s.T[1][1] = vals[4] * scale_fac; s.T[1][2] = vals[5] * scale_fac;
-                s.T[2][0] = vals[6] * scale_fac; s.T[2][1] = vals[7] * scale_fac; s.T[2][2] = vals[8] * scale_fac;
+                s.time = t * tt_to_in_time + TTOFFSET_ * (vscale_ / rscale_);
+                s.T[0][0] = vals[0]*tens_scale; s.T[0][1] = vals[1]*tens_scale; s.T[0][2] = vals[2]*tens_scale;
+                s.T[1][0] = vals[3]*tens_scale; s.T[1][1] = vals[4]*tens_scale; s.T[1][2] = vals[5]*tens_scale;
+                s.T[2][0] = vals[6]*tens_scale; s.T[2][1] = vals[7]*tens_scale; s.T[2][2] = vals[8]*tens_scale;
                 snapshots.push_back(s);
             } else {
                 double t2,a,b,c,d,e,f;
-                int n7 = std::sscanf(linebuf, "%lf %lf %lf %lf %lf %lf %lf", &t2,&a,&b,&c,&d,&e,&f);
+                int n7 = std::sscanf(linebuf, "%lf %lf %lf %lf %lf %lf %lf",
+                                     &t2,&a,&b,&c,&d,&e,&f);
                 if (n7==7) {
-                    s.time = t2 * (TTUNIT / TSTAR) + (TTOFFSET / TSTAR);
-                    s.T[0][0] = a * scale_fac; s.T[0][1] = b * scale_fac; s.T[0][2] = c * scale_fac;
-                    s.T[1][0] = b * scale_fac; s.T[1][1] = d * scale_fac; s.T[1][2] = e * scale_fac;
-                    s.T[2][0] = c * scale_fac; s.T[2][1] = e * scale_fac; s.T[2][2] = f * scale_fac;
+                    s.time = t2 * tt_to_in_time + TTOFFSET_ * (vscale_ / rscale_);
+                    s.T[0][0] = a*tens_scale; s.T[0][1] = b*tens_scale; s.T[0][2] = c*tens_scale;
+                    s.T[1][0] = b*tens_scale; s.T[1][1] = d*tens_scale; s.T[1][2] = e*tens_scale;
+                    s.T[2][0] = c*tens_scale; s.T[2][1] = e*tens_scale; s.T[2][2] = f*tens_scale;
                     snapshots.push_back(s);
                 } else {
-                    std::cerr << "[TT] WARNING: unrecognized line format in tt.dat, skipping line:\n  '"
-                              << linebuf << "'\n";
+                    std::cerr << "[TT] WARNING: unrecognized tt.dat line, skipping:\n  " << linebuf;
                     --k; continue;
                 }
             }
         }
-
         std::fclose(fp);
 
         if (snapshots.empty()) {
@@ -132,65 +131,50 @@ public:
             return false;
         }
 
-        std::sort(snapshots.begin(), snapshots.end(), [](const TensorSnapshot &a, const TensorSnapshot &b){ return a.time < b.time; });
+        std::sort(snapshots.begin(), snapshots.end(),
+                  [](const TensorSnapshot &a, const TensorSnapshot &b){ return a.time < b.time; });
 
-        // initialise interp to first snapshot
-        interp = snapshots.front();
-        last_query_time = interp.time;
-        loaded = true;
+        interp_ = snapshots.front();
+        last_query_time_ = interp_.time;
+        loaded_ = true;
 
         std::cerr << "[TT] Loaded " << snapshots.size() << " snapshots; time range: "
                   << snapshots.front().time << " -> " << snapshots.back().time << " (PeTar units)\n"
-                  << "      TTUNIT=" << TTUNIT << ", TTOFFSET=" << TTOFFSET << ", TSTAR=" << TSTAR << "\n"
-                  << "      Tensor scale factor=" << scale_fac << "\n";
+                  << "      NBTT="<<NBTT_<<", TTUNIT="<<TTUNIT_<<", TTOFFSET="<<TTOFFSET_<<"\n"
+                  << "      scales: rscale="<<rscale_<<", vscale="<<vscale_<<"\n";
         return true;
     }
 
     // Set the reference point r0(t). Default is origin.
-    void setReference(const PS::F64vec &r0) { ref_point = r0; }
+    void setReference(const PS::F64vec &r0) { ref_point_ = r0; }
 
-    // Update internal interpolated tensor for the requested time.
-    // This does linear interpolation and clamps outside the range.
     void update(double time) {
-        if (!loaded) return;
-        // quick path: same time
-        if (time == last_query_time) return;
+        if (!loaded_) return;
+        if (time == last_query_time_) return;
 
-        // before first
-        if (time <= snapshots.front().time) {
-            interp = snapshots.front(); interp.time = time; last_query_time = time; return;
-        }
-        // after last
-        if (time >= snapshots.back().time) {
-            interp = snapshots.back(); interp.time = time; last_query_time = time; return;
-        }
+        if (time <= snapshots.front().time) { interp_ = snapshots.front(); interp_.time = time; last_query_time_=time; return; }
+        if (time >= snapshots.back().time)  { interp_ = snapshots.back();  interp_.time = time; last_query_time_=time; return; }
 
-        // find interval
-        auto it = std::upper_bound(snapshots.begin(), snapshots.end(), time,
-                                   [](double t, const TensorSnapshot &s){ return t < s.time; });
-        // it points to first snapshot with time > t, so prev is <= t
-        auto it_hi = it;
-        auto it_lo = it - 1;
-        double t_lo = it_lo->time;
-        double t_hi = it_hi->time;
-        double alpha = (time - t_lo) / (t_hi - t_lo);
+        auto it_hi = std::upper_bound(snapshots.begin(), snapshots.end(), time,
+                                      [](double t, const TensorSnapshot &s){ return t < s.time; });
+        auto it_lo = it_hi - 1;
+        double t_lo = it_lo->time, t_hi2 = it_hi->time;
+        double alpha = (time - t_lo) / (t_hi2 - t_lo);
 
-        interp.time = time;
-        for (int i=0;i<3;i++){
-            for (int j=0;j<3;j++){
-                interp.T[i][j] = (1.0 - alpha) * it_lo->T[i][j] + alpha * it_hi->T[i][j];
-            }
-        }
-        last_query_time = time;
+        interp_.time = time;
+        for (int i=0;i<3;i++) for (int j=0;j<3;j++)
+            interp_.T[i][j] = (1.0 - alpha)*it_lo->T[i][j] + alpha*it_hi->T[i][j];
+
+        last_query_time_ = time;
     }
 
     // Apply interpolated tensor to a position (pos - ref_point)
     PS::F64vec applyTensor(const PS::F64vec &pos) const {
-        PS::F64vec rel = pos - ref_point;
+        PS::F64vec rel = pos - ref_point_;
         PS::F64vec a;
-        a.x = interp.T[0][0]*rel.x + interp.T[0][1]*rel.y + interp.T[0][2]*rel.z;
-        a.y = interp.T[1][0]*rel.x + interp.T[1][1]*rel.y + interp.T[1][2]*rel.z;
-        a.z = interp.T[2][0]*rel.x + interp.T[2][1]*rel.y + interp.T[2][2]*rel.z;
+        a.x = interp_.T[0][0]*rel.x + interp_.T[0][1]*rel.y + interp_.T[0][2]*rel.z;
+        a.y = interp_.T[1][0]*rel.x + interp_.T[1][1]*rel.y + interp_.T[1][2]*rel.z;
+        a.z = interp_.T[2][0]*rel.x + interp_.T[2][1]*rel.y + interp_.T[2][2]*rel.z;
         return a;
     }
 
@@ -204,14 +188,47 @@ public:
     }
 
     bool getTimeRange(double &tmin, double &tmax) const {
-        if (!loaded) return false;
+        if (!loaded_) return false;
         tmin = snapshots.front().time; tmax = snapshots.back().time; return true;
     }
-
     size_t size() const { return snapshots.size(); }
-
-    double getTTUnit() const { return TTUNIT; }
-    double getTTOffset() const { return TTOFFSET; }
-    double getScaleFactor() const { return scale_fac; }
 };
 
+// IO layer (mirrors Galpy IO conventions)
+class IOParamsExternalTensor {
+public:
+    IOParams<std::string> mode;      // "none" | "galpy" | "tidal_tensor" (shared key)
+    IOParams<std::string> fname_tt;  // tt.dat filename
+    IOParams<double>      rscale;    // IN->TT length scale (1.0 default)
+    IOParams<double>      vscale;    // IN->TT velocity scale (1.0 default)
+
+    IOParamsExternalTensor(std::vector<void*> &input_par_store)
+    : mode     (input_par_store, "none", "external-force-mode", "Type of external force: none, galpy, tidal_tensor"),
+      fname_tt (input_par_store, "tt.dat", "tidal-tensor-file", "Filename for tidal tensor data"),
+      rscale   (input_par_store, 1.0, "tt-rscale", "Length scale from IN to TT unit"),
+      vscale   (input_par_store, 1.0, "tt-vscale", "Velocity scale from IN to TT unit")
+    {}
+};
+
+// High-level manager used by PeTar (like GalpyManager)
+class ExternalTensorManager {
+    TidalTensorManager tt_;
+public:
+    bool initial(const IOParamsExternalTensor &io, bool print_flag=true) {
+        tt_.setScales(io.rscale.value, io.vscale.value);
+        bool ok = tt_.loadFromFile(io.fname_tt.value);
+        if (!ok) return false;
+        if (print_flag) {
+            double tmin, tmax;
+            if (tt_.getTimeRange(tmin, tmax)) {
+                std::cerr << "[TT] Active. File='"<<io.fname_tt.value<<"', time range "<<tmin<<" to "<<tmax<<"\n";
+            }
+        }
+        return true;
+    }
+
+    void setReference(const PS::F64vec &r0) { tt_.setReference(r0); }
+    void update(double t_now)               { tt_.update(t_now); }
+    PS::F64vec accel(const PS::F64vec &pos) const { return tt_.applyTensor(pos); }
+    bool isLoaded() const { return tt_.isLoaded(); }
+};
