d59acbfd49a188d353fed6067d601969d8630fe4
Elizabeth <effymorh@Elizabeth.local>
2026-01-19 14:52:52 +0800

    mirroring galpy interface to connect with petar.hpp

--- src/external_tensor_force.hpp
+++ src/external_tensor_force.hpp
@@ -40,9 +40,10 @@ private:
 public:
     TidalTensorManager() {
         interp_.time = 0.0;
-        for (int i=0;i<3;i++) for(int j=0;j<3;j++) interp_.T[i][j]=0.0;
+        for (int i=0; i<3; i++) 
+            for (int j=0; j<3; j++) 
+                interp_.T[i][j] = 0.0;
     }
-
     void setScales(double rscale, double vscale) {
         rscale_ = rscale;
         vscale_ = vscale;
@@ -186,35 +187,246 @@ public:
     IOParams<double>      vscale;
 
     IOParamsExternalTensor(IOParamsContainer &store)
-    : fname_tt(store, "tt.dat", "tidal-tensor-file", "Filename for tidal tensor data"),
-      rscale  (store, 1.0,     "tt-rscale",         "Length scale from IN to TT"),
-      vscale  (store, 1.0,     "tt-vscale",         "Velocity scale from IN to TT")
-    {}
+        : fname_tt(store, "tt.dat", "tidal-tensor-file", "Filename for tidal tensor data"),
+          rscale(store, 1.0, "tt-rscale", "Length scale from IN to TT"),
+          vscale(store, 1.0, "tt-vscale", "Velocity scale from IN to TT") {}
 };
 
 // High-level manager used by PeTar (like GalpyManager)
 class ExternalTensorManager {
-    TidalTensorManager tt_;
-public:
-    bool initial(const IOParamsExternalTensor &io, bool print_flag=true) {
-        tt_.setScales(io.rscale.value, io.vscale.value);
-        bool ok = tt_.loadFromFile(io.fname_tt.value);
-        if (!ok) return false;
-        if (print_flag) {
-            double tmin, tmax;
-            if (tt_.getTimeRange(tmin, tmax)) {
-                std::cerr << "[TT] Active. File='"<<io.fname_tt.value<<"', time range "<<tmin<<" to "<<tmax<<"\n";
-            }
+private:
+    // Similar to Galpy's labelCheck
+    template <class Tstream>
+    void labelCheck(Tstream& fconf, const char* match) {
+        std::string label;
+        fconf>>label;
+        if (label!=match) {
+            std::cerr << "ExternalTensor config: reading label error, should be " 
+                      << match << " given " << label << std::endl;
+            abort();
         }
-        return true;
     }
 
-    void setReference(const PS::F64vec &r0) { tt_.setReference(r0); }
-    void update(double t_now)               { tt_.update(t_now);    }
-    PS::F64vec accel(const PS::F64vec &pos) const {
-    PS::F64vec a = tt_.applyTensor(pos);
-    std::cerr << "[TT] Position: " << pos.x << " " << pos.y << " " << pos.z 
-              << " -> Acceleration: " << -a.x << " " << -a.y << " " << -a.z << "\n";
-    return PS::F64vec(-a.x, -a.y, -a.z);}
-    bool isLoaded() const { return tt_.isLoaded(); }
+    void eofCheck(std::ifstream& fconf, const char* message) {
+        if (fconf.eof()) {
+            std::cerr << "ExternalTensor config: reading " << message 
+                     << " fails! File reaches EOF." << std::endl;
+            abort();
+        }
+    }
+
+    //! resize array by insert or erase elements for given offset index
+    /*!
+      @param[in] n_diff: size difference after update
+      @param[in] index: index of array offset for change
+      @param[in,out] array: array of data
+      @param[in,out] array_offset: offset of differert data groups 
+     */
+    template <class ttype>
+    void resizeArray(const int n_diff, const int index, std::vector<ttype>& array, std::vector<int>& array_offset) {
+        int offset = array_offset[index];
+        if (n_diff!=0) {
+            if (n_diff>0) {
+                std::vector<ttype> data(n_diff,ttype());
+                array.insert(array.begin()+offset, data.begin(), data.end());
+            }
+            else 
+                array.erase(array.begin()+offset, array.begin()+offset-n_diff);
+            for (size_t i=index+1; i<array_offset.size(); i++) 
+                array_offset[i] += n_diff;
+        }
+    }
+
+    //! erase array for one offset index
+    /*!
+      @param[in] index: index of array offset for remove
+      @param[in,out] array: array of data
+      @param[in,out] array_offset: offset of differert data groups 
+    */
+    template <class ttype>
+    void eraseArray(const int index, std::vector<ttype>& array, std::vector<int>& array_offset) {
+        int n = array_offset[index+1] - array_offset[index];
+        int offset = array_offset[index];
+        if (n>0) {
+            array.erase(array.begin()+offset, array.begin()+offset+n);
+        }
+        for (size_t i=index+1; i<array_offset.size(); i++) 
+            array_offset[i] = array_offset[i+1]-n;
+        array_offset.pop_back();
+        assert(array_offset.back()==int(array.size()));
+    }
+public:
+    // for MPI communication, data IO and initialization
+    std::vector<int> pot_type_offset; //  set offset
+    std::vector<int> pot_type;    // types for each set
+    std::vector<int> pot_args_offset; // arguments of pot for each set
+    std::vector<double> pot_args;    // set offset
+    double time;   // current time, update in evolveChangingArguments
+    std::vector<ChangeArgument> change_args;  // changing argument index to evolve
+    std::vector<int> change_args_offset; // set offset
+    // tt potential arguments
+    std::vector<PotentialSetPar> pot_set_pars; // potential parameters for each set
+    std::vector<PotentialSet> pot_sets; // potential arguments for each set
+    double update_time;
+    // unit scaling
+    double rscale;
+    double vscale;
+    double tscale;
+    double fscale;
+    double pscale;
+    double gmscale;
+
+    ExternalTensorManager(): pot_type_offset(), pot_type(), 
+                    pot_args_offset(), pot_args(), change_args(), change_args_offset(),
+                    pot_set_pars(), pot_sets(), update_time(0.0), rscale(1.0), vscale(1.0), tscale(1.0), fscale(1.0), pscale(1.0), gmscale(1.0), fconf(), set_name(), set_parfile() {}
+
+    //! print current potential data
+    void printData(std::ostream& fout) {
+        fout<<"tt parameters, time: "<<time;
+        fout<<" Next update time: "<<update_time;
+        fout<<std::endl;
+        int nset = pot_set_pars.size();
+        for (int k=0; k<nset; k++) {
+            auto& pot_set_par_k = pot_set_pars[k];
+            fout<<"Potential set "<<k+1<<" Mode: "<<pot_set_par_k.mode
+                <<" GM: "<<pot_set_par_k.gm
+                <<" Pos: "<<pot_set_par_k.pos[0]<<" "<<pot_set_par_k.pos[1]<<" "<<pot_set_par_k.pos[2]
+                <<" Vel: "<<pot_set_par_k.vel[0]<<" "<<pot_set_par_k.vel[1]<<" "<<pot_set_par_k.vel[2]
+                <<" Acc: "<<pot_set_par_k.acc[0]<<" "<<pot_set_par_k.acc[1]<<" "<<pot_set_par_k.acc[2]
+                <<"\nPotential type indice: ";
+            for (int i=pot_type_offset[k]; i<pot_type_offset[k+1]; i++) 
+                fout<<pot_type[i]<<" ";
+            fout<<"\nPotential arguments: ";
+            for (int i=pot_args_offset[k]; i<pot_args_offset[k+1]; i++) 
+                fout<<pot_args[i]<<" ";
+            fout<<"\nChange argument [index mode rate]:";
+            for (int i=change_args_offset[k]; i<change_args_offset[k+1]; i++) {
+                fout<<"["<<change_args[i].index
+                    <<" "<<change_args[i].mode
+                    <<" "<<change_args[i].rate<<"] ";
+            }
+            fout<<std::endl;
+        }
+    }        
+
+#ifdef PARTICLE_SIMULATOR_MPI_PARALLEL        
+    void broadcastDataMPI() {
+
+        int nset;
+        int my_rank = PS::Comm::getRank();
+        if (my_rank==0) nset = pot_set_pars.size();
+        PS::Comm::broadcast(&nset, 1, 0);
+
+        // update potentials
+        if (nset>=0) {
+
+            PS::Comm::broadcast(&update_time, 1, 0);
+            if (my_rank==0) {
+                assert((int)pot_type_offset.size()==nset+1);
+                assert((int)pot_args_offset.size()==nset+1);
+                assert((int)change_args_offset.size()==nset+1);
+            }
+            else {
+                pot_set_pars.resize(nset);
+                pot_type_offset.resize(nset+1);
+                pot_args_offset.resize(nset+1);
+                change_args_offset.resize(nset+1);
+            }
+            PS::Comm::broadcast(pot_set_pars.data(), pot_set_pars.size(), 0);
+            PS::Comm::broadcast(pot_type_offset.data(), pot_type_offset.size(), 0);
+            PS::Comm::broadcast(pot_args_offset.data(), pot_args_offset.size(), 0);
+            PS::Comm::broadcast(change_args_offset.data(), change_args_offset.size(), 0);
+            
+            if (my_rank==0) {
+                assert((int)pot_type.size()==pot_type_offset.back());
+                assert((int)pot_args.size()==pot_args_offset.back());
+                assert((int)change_args.size()==change_args_offset.back());
+            }
+            else {
+                pot_type.resize(pot_type_offset.back());
+                pot_args.resize(pot_args_offset.back());
+                change_args.resize(change_args_offset.back());
+            }
+            PS::Comm::broadcast(pot_type.data(), pot_type.size(), 0);
+            if (pot_args.size()>0) PS::Comm::broadcast(pot_args.data(), pot_args.size(), 0);
+            if (change_args.size()>0) PS::Comm::broadcast(change_args.data(), change_args.size(), 0);
+        }
+    }
+
+    //! calculate acceleration and potential at give position
+    /*!
+      @param[out] acc: [3] acceleration to return
+      @param[out] pot: potential to return 
+      @param[in] _time: time in input unit
+      @param[in] gm: G*mass of particles [input unit]
+      @param[in] pos_g: position of particles in the galactic frame [input unit]
+      @param[in] pos_l: position of particles in the particle system frame [input unit]
+     */
+    void calcAccPot(double* acc, double& pot, const double _time, const double gm, const double* pos_g, const double* pos_l) {
+        assert(pot_sets.size()==pot_set_pars.size());
+        int nset = pot_sets.size();
+        if (nset>0) {
+            double t = _time*tscale;
+
+            // galactic frame and rest frame of particle system
+            double x[2] = {pos_g[0]*rscale, pos_l[0]*rscale};
+            double y[2] = {pos_g[1]*rscale, pos_l[1]*rscale};
+            double z[2] = {pos_g[2]*rscale, pos_l[2]*rscale};
+
+            pot = 0;
+            acc[0] = acc[1] = acc[2] = 0.0;
+
+
+            for (int k=0; k<nset; k++) {
+                int mode_k = pot_set_pars[k].mode;
+                assert(mode_k>=0||mode_k<=2);
+                int npot = pot_sets[k].npot;
+                double* pos_k = pot_set_pars[k].pos;
+                int i = (mode_k & 1); // get first bit to select frame (0: galactic; 1: rest)
+                // frame is consistent 
+                double dx = x[i]-pos_k[0];
+                double dy = y[i]-pos_k[1];
+                double dz = z[i]-pos_k[2];
+                double rxy= std::sqrt(dx*dx+dy*dy);
+                double phi= std::atan2(dy, dx);
+                double sinphi = dy/rxy;
+                double cosphi = dx/rxy;
+
+                auto& pot_args = pot_sets[k].arguments;
+                double acc_rxy = calcRforce(rxy, dz, phi, t, npot, pot_args);
+                double acc_z   = calczforce(rxy, dz, phi, t, npot, pot_args);
+                double pot_i = evaluatePotentials(rxy, dz, npot, pot_args);
+                double gm_pot = pot_set_pars[k].gm;
+                if (rxy>0.0) {
+                    assert(!std::isinf(acc_rxy));
+                    assert(!std::isnan(acc_rxy));
+                    assert(!std::isinf(acc_phi));
+                    assert(!std::isnan(acc_phi));
+                    assert(!std::isinf(pot));
+                    assert(!std::isnan(pot));
+                    pot += pot_i;
+                    double acc_x = (cosphi*acc_rxy - sinphi*acc_phi/rxy);
+                    double acc_y = (sinphi*acc_rxy + cosphi*acc_phi/rxy);
+                    acc[0] += acc_x;
+                    acc[1] += acc_y;
+                    acc[2] += acc_z;
+                    if (mode_k==2) {
+                        double* acc_pot = pot_set_pars[k].acc;
+                        acc_pot[0] -= gm*acc_x/gm_pot; // anti-acceleration to potential set origin
+                        acc_pot[1] -= gm*acc_y/gm_pot;
+                        acc_pot[2] -= gm*acc_z/gm_pot;
+                    }
+                }
+            }
+            pot /= pscale;
+            acc[0] /= fscale;
+            acc[1] /= fscale;
+            acc[2] /= fscale;
+
+            //pot = acc[0]*x+acc[1]*y+acc[2]*z;
+        }
+        else {
+            acc[0] = acc[1] = acc[2] = pot = 0.0;
+        }
+    }
 };
